9a77df5cf1afbdbbb39150849c8cf3341327d621
Your Name <you@example.com>
2015-07-11 01:54:40 +0000

    Added unit test for KV.Bucket.delete

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -12,4 +12,14 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "delete key/value by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "delete_me") == nil
+
+    KV.Bucket.put(bucket, "delete_me", 1)
+    assert KV.Bucket.get(bucket, "delete_me") == 1
+
+    KV.Bucket.delete(bucket, "delete_me")
+    assert KV.Bucket.get(bucket, "delete_me") == nil
+  end
 end
